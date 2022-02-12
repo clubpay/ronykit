12234ca9e1a2a3058060340ff9180e076ab6b860
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-02-13 00:24:07 +0400

    cleanup and more comments

--- desc/desc.go
+++ desc/desc.go
@@ -18,30 +18,41 @@ func NewContract() *Contract {
 	return &Contract{}
 }
 
+// SetName sets the name of the Contract c, it MUST be unique per Service. However, it
+// has no operation effect only is helpful with some other tools such as monitoring,
+// logging or tracing tools to identity it.
 func (c *Contract) SetName(name string) *Contract {
 	c.Name = name
 
 	return c
 }
 
+// SetInput sets the accepting message for this Contract. Contracts are bound to one input message.
+// In some odd cases if you need to handle multiple input messages, then you SHOULD create multiple
+// contracts but with same handlers and/or selectors.
 func (c *Contract) SetInput(m ronykit.Message) *Contract {
 	c.Input = m
 
 	return c
 }
 
+// SetOutput sets the outgoing message for this Contract. This is an optional parameter, which
+// mostly could be used by external tools such as Swagger generator etc.
 func (c *Contract) SetOutput(m ronykit.Message) *Contract {
 	c.Output = m
 
 	return c
 }
 
+// AddSelector adds a ronykit.RouteSelector for this contract. Selectors are bundle specific.
 func (c *Contract) AddSelector(s ronykit.RouteSelector) *Contract {
 	c.Selectors = append(c.Selectors, s)
 
 	return c
 }
 
+// AddModifier ass a ronykit.Modifier for this contract. Modifiers are used to modify
+// the outgoing ronykit.Envelope just before sending to the client.
 func (c *Contract) AddModifier(m ronykit.Modifier) *Contract {
 	c.Modifiers = append(c.Modifiers, m)
 
@@ -83,6 +94,7 @@ type Service struct {
 	Name         string
 	Version      string
 	Description  string
+	Wrappers     []ronykit.ServiceWrapper
 	Contracts    []Contract
 	PreHandlers  []ronykit.Handler
 	PostHandlers []ronykit.Handler
@@ -94,10 +106,23 @@ func NewService(name string) *Service {
 	}
 }
 
-func (s *Service) Add(c *Contract) *Service {
-	if c != nil {
-		s.Contracts = append(s.Contracts, *c)
+func (s *Service) AddWrapper(wrappers ...ronykit.ServiceWrapper) {
+	s.Wrappers = append(s.Wrappers, wrappers...)
+}
+
+func (s *Service) AddContract(contracts ...*Contract) {
+	for idx := range contracts {
+		if contracts[idx] == nil {
+			continue
+		}
+		s.Contracts = append(s.Contracts, *contracts[idx])
 	}
+}
+
+// Add adds a contract to the service.
+// Deprecated use AddContract instead.
+func (s *Service) Add(c *Contract) *Service {
+	s.AddContract(c)
 
 	return s
 }
@@ -113,5 +138,5 @@ func (s Service) Generate() ronykit.Service {
 		svc.contracts = append(svc.contracts, c.Generate()...)
 	}
 
-	return svc
+	return ronykit.WrapService(svc, s.Wrappers...)
 }
