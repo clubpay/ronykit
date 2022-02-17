81ea562b479cb5438cec13312a9b337d9902570d
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-02-17 08:56:36 +0400

    fix panic in reflector package

---
+++ reflector/reflector_test.go
@@ -0,0 +1,28 @@
+package reflector_test
+
+import (
+	"github.com/clubpay/ronykit/reflector"
+	"testing"
+)
+
+type testMessage struct {
+	X string
+	Y int64
+	z string
+	M map[string]string
+}
+
+func (t testMessage) Marshal() ([]byte, error) {
+	return nil, nil
+}
+
+func TestReflector(t *testing.T) {
+	r := reflector.New()
+	m := &testMessage{
+		X: "xValue",
+		Y: 10,
+		z: "zValue",
+		M: nil,
+	}
+	t.Log(r.Get(m, "X"))
+}
