30424ece6cae1e88659c13ee9df555ca53f7c9b6
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-05-29 18:54:43 +0430

    refactor

--- ronykit.go
+++ ronykit.go
@@ -1,6 +1,33 @@
 package ronykit
 
-import "mime/multipart"
+import (
+	"context"
+	"mime/multipart"
+)
+
+// Bundle is the pluggable component of the EdgeServer.
+type Bundle interface {
+	// Start starts the gateway to accept connections.
+	Start(ctx context.Context) error
+	// Shutdown shuts down the gateway gracefully.
+	Shutdown(ctx context.Context) error
+	// Register registers the route in the Bundle. This is how Bundle get information
+	// about the services and their contracts.
+	Register(serviceName, contractID string, sel RouteSelector, input Message)
+}
+
+type (
+	ExecuteArg struct {
+		WriteFunc
+		ServiceName string
+		ContractID  string
+		Route       string
+	}
+	WriteFunc   func(conn Conn, e Envelope) error
+	ExecuteFunc func(arg ExecuteArg)
+)
+
+var NoExecuteArg = ExecuteArg{}
 
 // Conn represents a connection between EdgeServer and client.
 type Conn interface {
--- ronykit_test.go
+++ ronykit_test.go
@@ -3,7 +3,16 @@ package ronykit_test
 import (
 	"bytes"
 	"io/ioutil"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
+
+func TestRonykit(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Ronykit Suite")
+}
 
 type TestConn struct {
 	id       uint64
