accc19edee08aa22f3e2c63a7b6d3c04635cdf02
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-12-18 10:21:28 +0330

    [kit] fix sha signature in utils package

--- kit/utils/hash.go
+++ kit/utils/hash.go
@@ -21,26 +21,31 @@ var poolSha512 = sync.Pool{
 	},
 }
 
-// Sha512 appends a 64bytes array which is sha512(in) to out
-func Sha512(in, out []byte) error {
+// Sha512 appends a 64bytes array which is sha512(in) to out and returns out
+func Sha512(in, out []byte) ([]byte, error) {
 	h := poolSha512.Get().(hash.Hash) //nolint:forcetypeassert
 	if _, err := h.Write(in); err != nil {
 		h.Reset()
 		poolSha512.Put(h)
 
-		return err
+		return out, err
 	}
-	h.Sum(out)
+	out = h.Sum(out)
 	h.Reset()
 	poolSha512.Put(h)
 
-	return nil
+	return out, nil
 }
 
-func MustSha512(in, out []byte) {
-	if err := Sha512(in, out); err != nil {
+// MustSha512 is Sha512 but it panics if any error happens
+func MustSha512(in, out []byte) []byte {
+	var err error
+	out, err = Sha512(in, out)
+	if err != nil {
 		panic(err)
 	}
+
+	return out
 }
 
 var poolSha256 = sync.Pool{
@@ -50,23 +55,27 @@ var poolSha256 = sync.Pool{
 }
 
 // Sha256 appends a 32bytes array which is sha256(in) to out
-func Sha256(in, out []byte) error {
+func Sha256(in, out []byte) ([]byte, error) {
 	h := poolSha256.Get().(hash.Hash) //nolint:forcetypeassert
 	if _, err := h.Write(in); err != nil {
 		h.Reset()
 		poolSha256.Put(h)
 
-		return err
+		return out, err
 	}
-	h.Sum(out)
+	out = h.Sum(out)
 	h.Reset()
 	poolSha256.Put(h)
 
-	return nil
+	return out, nil
 }
 
-func MustSha256(in, out []byte) {
-	if err := Sha256(in, out); err != nil {
+func MustSha256(in, out []byte) []byte {
+	var err error
+	out, err = Sha256(in, out)
+	if err != nil {
 		panic(err)
 	}
+
+	return out
 }
