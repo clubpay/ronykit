49c63539d45a8006a7f5666a436aa913676e1b3e
Burak Kuru <kurub16@itu.edu.tr>
2023-01-09 11:25:58 +0300

    TECH-2715/stripe-descriptor StrTruncate func

--- kit/utils/numstr.go
+++ kit/utils/numstr.go
@@ -125,6 +125,21 @@ func IntToStr(x int) string {
 	return strconv.FormatUint(uint64(x), 10)
 }
 
+func StrTruncate(s string, maxSize int) string {
+
+	truncated := ""
+	count := 0
+	for _, char := range s {
+		truncated += string(char)
+		count++
+		if count >= maxSize {
+			break
+		}
+	}
+	return truncated
+
+}
+
 // ByteToStr converts byte slice to a string without memory allocation.
 // Note it may break if string and/or slice header will change
 // in the future go versions.
