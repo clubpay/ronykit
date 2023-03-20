43c859fc39f183de313beb38be29818128090f1d
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2023-03-20 12:33:54 +0300

    bump version to v0.10.21

--- updateversion.sh
+++ updateversion.sh
@@ -4,7 +4,7 @@
 ss="github.com/clubpay/ronykit/kit $1"
 rs="github.com/clubpay/ronykit/kit $2"
 
-array=( contrib std/gateways/fasthttp std/gateways/fastws std/gateways/silverhttp std/clusters/rediscluster )
+array=( std/gateways/fasthttp std/gateways/fastws std/gateways/silverhttp std/clusters/rediscluster )
 for i in "${array[@]}"
 do
 	filename="$i"/go.mod
@@ -24,9 +24,19 @@ git push
 git push --tags
 
 ## for contrib we need to update fasthttp too
-#ss="github.com/clubpay/ronykit/std/gateways/fasthttp $1"
-#rs="github.com/clubpay/ronykit/std/gateways/fasthttp $2"
-#filename="contrib/go.mod"
-#echo "update go.mod for [$filename]: $ss -> $rs"
-#sed -i'' -e 's#'"$ss"'#'"$rs"'#g' "$filename"
-#rm contrib/go.mod-e
+filename="contrib/go.mod"
+ss="github.com/clubpay/ronykit/kit $1"
+rs="github.com/clubpay/ronykit/kit $2"
+echo "update go.mod for [$filename]: $ss -> $rs"
+sed -i'' -e 's#'"$ss"'#'"$rs"'#g' "$filename"
+ss="github.com/clubpay/ronykit/std/gateways/fasthttp $1"
+rs="github.com/clubpay/ronykit/std/gateways/fasthttp $2"
+echo "update go.mod for [$filename]: $ss -> $rs"
+sed -i'' -e 's#'"$ss"'#'"$rs"'#g' "$filename"
+rm contrib/go.mod-e
+
+git add .
+git commit -m "[contrib] bump version to $2"
+git tag -a "contrib/$2" -m "$2"
+git push
+git push --tags
