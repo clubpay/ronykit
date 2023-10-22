1b5043a626c0b72ce5455facafb0dc7ece1c09c8
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2023-10-22 16:25:53 +0300

    [gateway] add more test cases

--- std/gateways/fasthttp/selector_test.go
+++ std/gateways/fasthttp/selector_test.go
@@ -13,11 +13,12 @@ func TestConvertLegacyPath(t *testing.T) {
 		"/:x/:y/:z":                   "/{x}/{y}/{z}",
 		"/something/:x/another/:y":    "/something/{x}/another/{y}",
 		"/x/:y/:z":                    "/x/{y}/{z}",
+		"/some:thing/another/:x":      "/some:thing/another/{x}",
+		"/this/x:this/{y:this}":       "/this/x:this/{y:this}",
 	}
 	for k := range in {
 		if convertLegacyPathFormat(k) != in[k] {
 			t.Fatalf("expected: %s, got: %s", in[k], convertLegacyPathFormat(k))
 		}
 	}
-
 }
