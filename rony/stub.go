8b520327d0283b10a61057cd5d9cb0ca94eae13c
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2024-06-30 16:11:32 +0300

    [rony] add helper method to generate stub

---
+++ rony/stub.go
@@ -0,0 +1,26 @@
+package rony
+
+import "github.com/clubpay/ronykit/stub/stubgen"
+
+// GenerateStub generates a stub file for the given service description.
+// The generated file will be placed in the `outputDir/folderName` directory.
+// The package name of the generated file will be `pkgName`.
+// The struct holding the stub functions will be named `name`.
+func GenerateStub[S State[A], A Action](
+	name, folderName, outputDir, pkgName string,
+	genFunc stubgen.GenFunc, ext string,
+	opt ...SetupOption[S, A],
+) error {
+	ctx := SetupContext[S, A]{}
+	for _, o := range opt {
+		o(&ctx)
+	}
+
+	return stubgen.New(
+		stubgen.WithStubName(name),
+		stubgen.WithFolderName(folderName),
+		stubgen.WithOutputDir(outputDir),
+		stubgen.WithPkgName(pkgName),
+		stubgen.WithGenFunc(genFunc, ext),
+	).Generate(ctx.cfg.allServiceDesc()...)
+}
