155a1ccdaee22357f561d5506bfc51190a9671ac
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2025-04-09 15:06:05 +0300

    Remove unused testcontainers logging configuration
    The initialization code for testcontainers logging was removed as it is no longer used. This cleanup simplifies the codebase and eliminates unnecessary dependencies.

--- boxship/boxship.go
+++ boxship/boxship.go
@@ -1,13 +1,4 @@
 package boxship
 
-import (
-	"io"
-	"log"
-
-	"github.com/testcontainers/testcontainers-go"
-)
-
 //go:generate go run ./update_version.go
-func init() {
-	testcontainers.Logger = log.New(io.Discard, "", 0)
-}
+func init() {}
