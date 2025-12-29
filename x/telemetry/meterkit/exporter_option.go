f9e4c32dbe7d6d258c9a9f5a107ad0c31ca22a1a
ehsan <ehsan@ronaksoft.com>
2025-12-30 00:03:51 +0300

    refactor: use dedicated ServeMux for Prometheus exporter to improve routing clarity

--- x/telemetry/meterkit/exporter_option.go
+++ x/telemetry/meterkit/exporter_option.go
@@ -7,6 +7,7 @@ import (
 	prom "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/otel/exporters/prometheus"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
 type ExporterOption func(e *Exporter) error
@@ -30,12 +31,18 @@ func WithPrometheus(path string, port int, opt ...prometheus.Option) ExporterOpt
 		e.shutdownFn = exp.Shutdown
 
 		go func() {
-			http.Handle(path, promhttp.HandlerFor(prom.DefaultGatherer, promhttp.HandlerOpts{}))
-			err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil) // nosemgrep
-			if err != nil {
-				panic(err)
-			}
+			mux := http.NewServeMux()
+			mux.Handle(path, promhttp.HandlerFor(prom.DefaultGatherer, promhttp.HandlerOpts{}))
+			_ = http.ListenAndServe(fmt.Sprintf(":%d", port), mux) // nosemgrep
 		}()
+
+		return nil
+	}
+}
+
+func WithMetricReader(rd sdkmetric.Reader) ExporterOption {
+	return func(e *Exporter) error {
+		e.rd = rd
 
 		return nil
 	}
