2a18775f3a539d9c3f19ebbc2a49fd70702de155
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2026-01-17 00:26:44 +0300

    add `workspace` and `service` setup commands: refactor workspace structure, update templates, and introduce support for `i18n`

--- ronyup/cmd/setup/setup.go
+++ ronyup/cmd/setup/setup.go
@@ -27,27 +27,42 @@ var opt = struct {
 }{}
 
 func init() {
-	flagSet := Cmd.Flags()
-	flagSet.StringVarP(&opt.RepositoryRootDir, "repoDir", "d", "./my-repo", "destination directory for the setup")
-	flagSet.StringVarP(&opt.RepositoryGoModule, "repoModule", "r", "github.com/your/repo", "go module for the repository")
-	flagSet.StringVarP(
+	rootFlagSet := Cmd.PersistentFlags()
+	rootFlagSet.StringVarP(
+		&opt.RepositoryRootDir,
+		"repoDir",
+		"d",
+		"./my-repo",
+		"destination directory for the setup",
+	)
+	rootFlagSet.StringVarP(
+		&opt.RepositoryGoModule,
+		"repoModule",
+		"r",
+		"github.com/your/repo",
+		"go module for the repository",
+	)
+	rootFlagSet.BoolVarP(&opt.Force, "force", "f", false, "clean destination directory before setup")
+	rootFlagSet.StringToStringVarP(&opt.Custom, "custom", "c", map[string]string{}, "custom values for the template")
+
+	projectFlagSet := CmdSetupProject.Flags()
+	projectFlagSet.StringVarP(
 		&opt.ProjectDir,
 		"projectDir",
 		"p",
 		"my-project",
 		"destination directory inside repoDir for the setup",
 	)
-	flagSet.StringVarP(&opt.ProjectName, "projectName", "n", "MyProject", "project name")
-
-	flagSet.BoolVarP(&opt.Force, "force", "f", false, "clean destination directory before setup")
-	flagSet.StringVarP(
+	projectFlagSet.StringVarP(&opt.ProjectName, "projectName", "n", "MyProject", "project name")
+	projectFlagSet.StringVarP(
 		&opt.Template,
 		"template",
 		"t",
 		"service",
-		"possible values: workspace | service",
+		"possible values: service | job",
 	)
-	flagSet.StringToStringVarP(&opt.Custom, "custom", "c", map[string]string{}, "custom values for the template")
+
+	Cmd.AddCommand(CmdSetupWorkspace, CmdSetupProject)
 }
 
 var Cmd = &cobra.Command{
@@ -59,18 +74,51 @@ var Cmd = &cobra.Command{
 			return err
 		}
 
-		err = createDestination(cmd.Context())
+		return nil
+	},
+}
+
+var CmdSetupWorkspace = &cobra.Command{
+	Use:                "workspace",
+	FParseErrWhitelist: cobra.FParseErrWhitelist{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := cmd.ParseFlags(args)
 		if err != nil {
 			return err
 		}
 
-		copyTemplate(cmd)
+		err = createWorkspace(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		copyWorkspaceTemplate(cmd)
 
 		return nil
 	},
 }
 
-func createDestination(ctx context.Context) error {
+var CmdSetupProject = &cobra.Command{
+	Use:                "service",
+	FParseErrWhitelist: cobra.FParseErrWhitelist{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := cmd.ParseFlags(args)
+		if err != nil {
+			return err
+		}
+
+		err = createProject(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		copyProjectTemplate(cmd)
+
+		return nil
+	},
+}
+
+func createWorkspace(_ context.Context) error {
 	// get the absolute path to the output directory
 	repoPath, err := filepath.Abs(opt.RepositoryRootDir)
 	if err != nil {
@@ -79,21 +127,24 @@ func createDestination(ctx context.Context) error {
 
 	_ = os.MkdirAll(repoPath, 0o755)
 
-	if opt.ProjectDir != "" {
-		p := z.RunCmdParams{Dir: repoPath}
-		z.RunCmd(ctx, p, "go", "work", "init")
+	return nil
+}
 
-		projectPath := filepath.Join(repoPath, opt.ProjectDir)
+func createProject(_ context.Context) error {
+	if opt.ProjectDir == "" {
+		return fmt.Errorf("project directory is required")
+	}
 
-		_ = os.MkdirAll(projectPath, 0o755)
-		if !z.IsEmptyDir(projectPath) {
-			if !opt.Force {
-				return fmt.Errorf("%s directory is not empty, use -f to force", projectPath)
-			}
+	projectPath := filepath.Join("feature", opt.ProjectDir)
 
-			rkit.Assert(os.RemoveAll(projectPath))
-			rkit.Assert(os.MkdirAll(projectPath, 0o755)))
+	_ = os.MkdirAll(projectPath, 0o755)
+	if !z.IsEmptyDir(projectPath) {
+		if !opt.Force {
+			return fmt.Errorf("%s directory is not empty, use -f to force", projectPath)
 		}
+
+		rkit.Assert(os.RemoveAll(projectPath))
+		rkit.Assert(os.MkdirAll(projectPath, 0o755))
 	}
 
 	return nil
@@ -109,9 +160,9 @@ type ModuleInput struct {
 	RonyKitPath string
 }
 
-func copyTemplate(cmd *cobra.Command) {
+func copyProjectTemplate(cmd *cobra.Command) {
 	pathPrefix := filepath.Join("skeleton", opt.Template)
-	packagePath := filepath.Join(opt.RepositoryRootDir, opt.ProjectDir)
+	packagePath := filepath.Join("feature", opt.ProjectDir)
 
 	rkit.Assert(
 		fs.WalkDir(
@@ -141,6 +192,55 @@ func copyTemplate(cmd *cobra.Command) {
 					TemplateSuffix: "tmpl",
 					TemplateInput: ModuleInput{
 						RepositoryPath: strings.TrimSuffix(opt.RepositoryGoModule, "/"),
+						PackagePath:    strings.Trim(path.Join("feature", opt.ProjectDir), "/"),
+						PackageName:    opt.ProjectName,
+						RonyKitPath:    "github.com/clubpay/ronykit",
+					},
+				})
+			}),
+	)
+
+	cmd.Println("Project created successfully")
+	cmd.Println("Project path:", packagePath)
+	p := z.RunCmdParams{Dir: filepath.Join("./feature", packagePath)}
+	z.RunCmd(cmd.Context(), p, "go", "mod", "init", path.Join(opt.RepositoryGoModule, "feature", opt.ProjectDir))
+	z.RunCmd(cmd.Context(), p, "go", "mod", "edit", "-go=1.25")
+	z.RunCmd(cmd.Context(), p, "go", "mod", "tidy")
+	z.RunCmd(cmd.Context(), p, "go", "fmt", "./...")
+	z.RunCmd(cmd.Context(), p, "go", "work", "use", ".")
+}
+
+func copyWorkspaceTemplate(cmd *cobra.Command) {
+	pathPrefix := filepath.Join("skeleton", "workspace")
+
+	rkit.Assert(
+		fs.WalkDir(
+			internal.Skeleton, pathPrefix,
+			func(currPath string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+
+				srcPath := strings.TrimPrefix(currPath, pathPrefix)
+				destPath := strings.TrimSuffix(
+					filepath.Join(".", opt.RepositoryRootDir, srcPath),
+					"tmpl",
+				)
+
+				if d.IsDir() {
+					// Create a directory if it doesn't exist
+					return os.MkdirAll(destPath, os.ModePerm)
+				}
+
+				cmd.Println("FILE: ", destPath, "created")
+
+				return z.Copy(z.CopyParams{
+					FS:             internal.Skeleton,
+					SrcPath:        currPath,
+					DestPath:       destPath,
+					TemplateSuffix: "tmpl",
+					TemplateInput: ModuleInput{
+						RepositoryPath: strings.TrimSuffix(opt.RepositoryGoModule, "/"),
 						PackagePath:    strings.Trim(opt.ProjectDir, "/"),
 						PackageName:    opt.ProjectName,
 						RonyKitPath:    "github.com/clubpay/ronykit",
@@ -149,12 +249,12 @@ func copyTemplate(cmd *cobra.Command) {
 			}),
 	)
 
-	cmd.Println("Module created successfully")
-	cmd.Println("Project path: ", packagePath)
-	p := z.RunCmdParams{Dir: filepath.Join(".", packagePath)}
-	z.RunCmd(cmd.Context(), p, "go", "mod", "init", path.Join(opt.RepositoryGoModule, opt.ProjectDir))
-	z.RunCmd(cmd.Context(), p, "go", "mod", "edit", "-go=1.23")
+	cmd.Println("Workspace created successfully")
+	p := z.RunCmdParams{Dir: filepath.Join(".", opt.RepositoryRootDir)}
+	z.RunCmd(cmd.Context(), p, "go", "work", "init")
+	p = z.RunCmdParams{Dir: filepath.Join(".", opt.RepositoryRootDir, "pkg/i18n")}
+	z.RunCmd(cmd.Context(), p, "go", "mod", "init", path.Join(opt.RepositoryGoModule, "pkg/i18n"))
+	z.RunCmd(cmd.Context(), p, "go", "mod", "edit", "-go=1.25")
 	z.RunCmd(cmd.Context(), p, "go", "mod", "tidy")
-	z.RunCmd(cmd.Context(), p, "go", "fmt", "./...")
 	z.RunCmd(cmd.Context(), p, "go", "work", "use", ".")
 }
--- ronyup/go.mod
+++ ronyup/go.mod
@@ -3,6 +3,7 @@ module github.com/clubpay/ronykit/ronyup
 go 1.25.1
 
 require (
+	github.com/Masterminds/sprig/v3 v3.3.0
 	github.com/clubpay/ronykit/x/rkit v0.2.9
 	github.com/spf13/cobra v1.10.2
 	golang.org/x/mod v0.32.0
@@ -11,12 +12,21 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.3.0 // indirect
 	github.com/goccy/go-reflect v1.2.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/huandu/xstrings v1.5.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/copier v0.4.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
