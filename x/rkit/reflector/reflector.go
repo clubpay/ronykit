8916f9e74c6c00bb128ad45d32ded1ff163d5149
Ehsan <ehsan.noureddin@gmail.com>
2026-09-20 12:25:21 +0300

    Add reflector utility for efficient struct field access and benchmarking

---
+++ x/rkit/reflector/reflector.go
@@ -0,0 +1,208 @@
+package reflector
+
+import (
+	"container/list"
+	"fmt"
+	"reflect"
+	"strings"
+	"sync"
+
+	"github.com/clubpay/ronykit/kit"
+)
+
+var (
+	ErrNotExported        = fmt.Errorf("not exported")
+	ErrNoField            = fmt.Errorf("field not exists")
+	ErrMessageIsNotStruct = fmt.Errorf("message is not a struct")
+)
+
+type Reflector struct {
+	cacheMtx sync.RWMutex
+	cache    map[reflect.Type]*Reflected
+}
+
+func New() *Reflector {
+	return &Reflector{
+		cache: map[reflect.Type]*Reflected{},
+	}
+}
+
+// Register registers the message, then reflector will be much faster. You should call
+// it concurrently.
+func Register(m kit.Message, tags ...string) {
+	if m == nil {
+		return
+	}
+
+	if len(tags) == 0 {
+		tags = []string{"json", "proto", "msg"}
+	}
+
+	mVal, err := getValue(m)
+	if err != nil {
+		return
+	}
+
+	mType := mVal.Type()
+	registered[mType] = destruct(mType, tags...)
+}
+
+func getValue(m kit.Message) (reflect.Value, error) {
+	mVal := reflect.Indirect(reflect.ValueOf(m))
+	if mVal.Kind() != reflect.Struct {
+		return reflect.Value{}, ErrMessageIsNotStruct
+	}
+
+	return mVal, nil
+}
+
+type destructInput struct {
+	t       reflect.Type
+	offset  uintptr
+	indexes []int
+}
+
+func destruct(mType reflect.Type, tags ...string) *Reflected { //nolint:gocognit
+	r := &Reflected{
+		obj:   Fields{},
+		byTag: map[string]Fields{},
+		typ:   mType,
+	}
+
+	ll := list.New()
+	ll.PushFront(destructInput{t: mType, offset: 0})
+
+	for ll.Len() > 0 {
+		mt := ll.Remove(ll.Back()).(destructInput) //nolint:forcetypeassert
+		for i := range mt.t.NumField() {
+			ft := mt.t.Field(i)
+			if ft.PkgPath != "" && !ft.Anonymous {
+				continue
+			}
+
+			idx := append(mt.indexes, i)
+			fi := FieldInfo{
+				idx:    idx,
+				f:      ft,
+				name:   ft.Name,
+				offset: mt.offset + ft.Offset,
+				typ:    ft.Type,
+			}
+
+			switch ft.Type.Kind() {
+			case reflect.Map, reflect.Slice, reflect.Pointer,
+				reflect.Interface, reflect.Array, reflect.Chan,
+				reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
+			default:
+				fi.unsafe = true
+			}
+
+			if _, ok := r.obj[fi.name]; !ok {
+				r.obj[fi.name] = fi
+			}
+
+			if fi.f.Anonymous {
+				ll.PushFront(destructInput{t: ft.Type, offset: ft.Offset, indexes: idx})
+
+				continue
+			}
+
+			for _, t := range tags {
+				v, ok := ft.Tag.Lookup(t)
+				if !ok {
+					continue
+				}
+
+				idx := strings.IndexByte(v, ',')
+				if idx != -1 {
+					v = v[:idx]
+				}
+
+				if r.byTag[t] == nil {
+					r.byTag[t] = Fields{}
+				}
+
+				if _, ok := r.byTag[t][v]; !ok {
+					r.byTag[t][v] = fi
+				}
+			}
+		}
+	}
+
+	return r
+}
+
+func (r *Reflector) Load(m kit.Message, tags ...string) *Reflected {
+	mType := reflect.Indirect(reflect.ValueOf(m)).Type()
+
+	cachedData := registered[mType]
+	if cachedData == nil {
+		r.cacheMtx.RLock()
+		cachedData = r.cache[mType]
+		r.cacheMtx.RUnlock()
+
+		if cachedData == nil {
+			cachedData = destruct(mType, tags...)
+
+			r.cacheMtx.Lock()
+			r.cache[mType] = cachedData
+			r.cacheMtx.Unlock()
+		}
+	}
+
+	return cachedData
+}
+
+func (r *Reflector) Get(m kit.Message, fieldName string) (any, error) {
+	e, err := getValue(m)
+	if err != nil {
+		return nil, err
+	}
+
+	f, ok := e.Type().FieldByName(fieldName)
+	if !ok {
+		return nil, ErrNoField
+	}
+
+	if f.PkgPath != "" {
+		return nil, ErrNotExported
+	}
+
+	return e.FieldByName(fieldName).Interface(), nil
+}
+
+func (r *Reflector) GetString(m kit.Message, fieldName string) (string, error) {
+	e, err := getValue(m)
+	if err != nil {
+		return "", err
+	}
+
+	f, ok := e.Type().FieldByName(fieldName)
+	if !ok {
+		return "", ErrNoField
+	}
+
+	if f.PkgPath != "" {
+		return "", ErrNotExported
+	}
+
+	return e.FieldByName(fieldName).String(), nil
+}
+
+func (r *Reflector) GetInt(m kit.Message, fieldName string) (int64, error) {
+	e, err := getValue(m)
+	if err != nil {
+		return 0, err
+	}
+
+	f, ok := e.Type().FieldByName(fieldName)
+	if !ok {
+		return 0, ErrNoField
+	}
+
+	if f.PkgPath != "" {
+		return 0, ErrNotExported
+	}
+
+	return e.FieldByName(fieldName).Int(), nil
+}
---
+++ x/rkit/reflector/reflector_test.go
@@ -0,0 +1,197 @@
+package reflector_test
+
+import (
+	"testing"
+
+	"github.com/clubpay/ronykit/x/rkit"
+	"github.com/clubpay/ronykit/x/rkit/reflector"
+
+	goreflect "github.com/goccy/go-reflect"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testMessage struct {
+	testEmbed1
+	testEmbed2
+
+	X string `json:"xTag" otherTag:"xOther"`
+	Y int64  `json:"yTag" otherTag:"yOther"`
+	z string
+	M map[string]string
+}
+
+type testEmbed1 struct {
+	YY int64  `json:"yy" otherTag:"yyOther"`
+	MM string `json:"mM" otherTag:"mMOther"`
+	Y  int64  `json:"y"`
+}
+
+type testEmbed2 struct {
+	YYY int64  `json:"yyy" otherTag:"yyyOther"`
+	MMM string `json:"mMM" otherTag:"mMMOther"`
+	Y   int64  `json:"y2"`
+}
+
+func TestReflector(t *testing.T) {
+	r := reflector.New()
+	m := &testMessage{
+		testEmbed1: testEmbed1{
+			YY: 123,
+			MM: "mM",
+			Y:  11,
+		},
+		testEmbed2: testEmbed2{
+			YYY: 1234,
+			MMM: "mMM",
+			Y:   12,
+		},
+		X: "xValue",
+		Y: 10,
+		z: "zValue",
+		M: map[string]string{
+			"x": "100",
+		},
+	}
+	rObj := r.Load(m, "json")
+
+	t.Run("Load by Struct Fields", func(t *testing.T) {
+		assert.Equal(t, int64(10), m.Y)
+		obj := rObj.Obj()
+
+		yy, err := obj.GetInt64(m, "YY")
+		require.NoError(t, err)
+		assert.Equal(t, int64(123), yy)
+
+		mm, err := obj.GetString(m, "MM")
+		require.NoError(t, err)
+		assert.Equal(t, "mM", mm)
+
+		yyy, err := obj.GetInt64(m, "YYY")
+		require.NoError(t, err)
+		assert.Equal(t, int64(1234), yyy)
+
+		mmm, err := obj.GetString(m, "MMM")
+		require.NoError(t, err)
+		assert.Equal(t, "mMM", mmm)
+
+		assert.Equal(t, m.X, obj.GetStringDefault(m, "X", ""))
+		assert.Equal(t, m.Y, obj.GetInt64Default(m, "Y", 0))
+		assert.Empty(t, obj.GetStringDefault(m, "z", ""))
+		assert.Equal(t, map[string]string{"x": "100"}, obj.Get(m, "M"))
+
+		y, err := obj.GetInt64(m, "Y")
+		require.NoError(t, err)
+		assert.Equal(t, int64(10), y)
+	})
+
+	t.Run("Load by ToJSON tag", func(t *testing.T) {
+		byTag, ok := rObj.ByTag("json")
+		assert.True(t, ok)
+		assert.Equal(t, int64(11), byTag.GetInt64Default(m, "y", 0))
+		assert.Equal(t, m.X, byTag.GetStringDefault(m, "xTag", ""))
+		assert.Equal(t, m.Y, byTag.GetInt64Default(m, "yTag", 0))
+		assert.Equal(t, m.YY, byTag.GetInt64Default(m, "yy", 0))
+		assert.Equal(t, m.YYY, byTag.GetInt64Default(m, "yyy", 0))
+		assert.Equal(t, m.MM, byTag.GetStringDefault(m, "mM", ""))
+		assert.Equal(t, m.MMM, byTag.GetStringDefault(m, "mMM", ""))
+		assert.Equal(t, "def", byTag.GetStringDefault(m, "z", "def"))
+	})
+}
+
+/*
+Benchmark results:
+
+cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
+BenchmarkReflector/unsafe-16            				15217726      78.51 ns/op     0 B/op   0 allocs/op
+BenchmarkReflector/unsafeRegistered-16          97216087      11.58 ns/op     0 B/op   0 allocs/op
+BenchmarkReflector/reflect-16                   30267793      37.32 ns/op    16 B/op   2 allocs/op
+BenchmarkReflector/ccyReflect-16                58138024      22.71 ns/op     8 B/op   1 allocs/op
+*/
+func BenchmarkReflector(b *testing.B) {
+	benchs := []struct {
+		name string
+		f    func(*testing.B)
+	}{
+		{"unsafe", benchUnsafe},
+		{"unsafeRegistered", benchUnsafeRegistered},
+		{"reflect", benchReflect},
+		{"ccyReflect", benchGoReflect},
+	}
+
+	for idx := range benchs {
+		b.ResetTimer()
+		b.ReportAllocs()
+		b.Run(benchs[idx].name, benchs[idx].f)
+	}
+}
+
+func benchUnsafe(b *testing.B) {
+	r := reflector.New()
+	b.RunParallel(func(pb *testing.PB) {
+		t := &testMessage{}
+		t.X = rkit.RandomID(5)
+		for pb.Next() {
+			obj := r.Load(t).Obj()
+			xR, err := obj.GetString(t, "X")
+			if err != nil {
+				b.Fatal(err)
+			}
+			if xR != t.X {
+				b.Fatal(xR, t.X)
+			}
+		}
+	})
+}
+
+func benchUnsafeRegistered(b *testing.B) {
+	r := reflector.New()
+	reflector.Register(&testMessage{})
+	b.RunParallel(func(pb *testing.PB) {
+		t := &testMessage{}
+		t.X = rkit.RandomID(5)
+		for pb.Next() {
+			obj := r.Load(t).Obj()
+			xR, err := obj.GetString(t, "X")
+			if err != nil {
+				b.Fatal(err)
+			}
+			if err != nil {
+				b.Fatal(err)
+			}
+			if xR != t.X {
+				b.Fatal(xR, t.X)
+			}
+		}
+	})
+}
+
+func benchReflect(b *testing.B) {
+	r := reflector.New()
+	b.RunParallel(func(pb *testing.PB) {
+		t := &testMessage{}
+		t.X = rkit.RandomID(5)
+		for pb.Next() {
+			xR, err := r.GetString(t, "X")
+			if err != nil {
+				b.Fatal(err)
+			}
+			if xR != t.X {
+				b.Fatal(xR, t.X)
+			}
+		}
+	})
+}
+
+func benchGoReflect(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		t := &testMessage{}
+		t.X = rkit.RandomID(5)
+		for pb.Next() {
+			xR := goreflect.Indirect(goreflect.ValueOf(t)).FieldByName("X").String()
+			if xR != t.X {
+				b.Fatal(xR, t.X)
+			}
+		}
+	})
+}
